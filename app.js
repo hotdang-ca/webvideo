e0c83dca778beb1056aea445ce0b462f478aa3ed
James Perih <james@hotdang.ca>
2020-08-05 12:44:44 -0600

    Formatting consistencies.

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ var app = express();
 app.use(express.static('.'));
 
 app.get('/', function (_, res) {
-  res.redirect('/videoCapture.html')
+    res.redirect('/videoCapture.html')
 })
 
-app.post('/', multipartMiddleware, function(req, res) {
-  let location = path.join(__dirname + '/uploads/', `${new Date().getTime()}.webm`);
-  
-  fs.rename(req.files.data.path, location,() => {
+app.post('/', multipartMiddleware, function (req, res) {
+    let location = path.join(__dirname + '/uploads/', `${new Date().getTime()}.webm`);
+
+    fs.rename(req.files.data.path, location, () => {
         res.status(200).send(`upload successful, file written to ${location}`);
-  });
+    });
 });
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 });
--- videoCapture.js
+++ videoCapture.js
@@ -1,74 +1,74 @@
 const record = document.getElementById('record')
 const stop = document.getElementById('stop')
 
-if (!navigator.mediaDevices){
-  alert('getUserMedia support required to use this page')
+if (!navigator.mediaDevices) {
+    alert('getUserMedia support required to use this page')
 }
 
 const chunks = []
 let onDataAvailable = (e) => {
-  chunks.push(e.data)
+    chunks.push(e.data)
 }
 
 // Not showing vendor prefixes.
 navigator.mediaDevices.getUserMedia({
-  audio: true,
-  video: {
-    width: { ideal: 320 },
-    height: { ideal: 240 }
-  }
+    audio: true,
+    video: {
+        width: { ideal: 320 },
+        height: { ideal: 240 }
+    }
 }).then((mediaStream) => {
-  const recorder = new MediaRecorder(mediaStream);
-  recorder.ondataavailable = onDataAvailable;
+    const recorder = new MediaRecorder(mediaStream);
+    recorder.ondataavailable = onDataAvailable;
 
-  const video = document.querySelector('video');
-  video.srcObject = mediaStream;
-  video.volume = 0.0; // don't re-play audio.
+    const video = document.querySelector('video');
+    video.srcObject = mediaStream;
+    video.volume = 0.0; // don't re-play audio.
 
-  record.onclick = () => {
-    recorder.start()
-    document.getElementById('status').innerHTML = 'recorder started'
-    console.log(recorder.state)
-    console.log('recorder started')
-  }
+    record.onclick = () => {
+        recorder.start()
+        document.getElementById('status').innerHTML = 'recorder started'
+        console.log(recorder.state)
+        console.log('recorder started')
+    }
 
-  stop.onclick = ()=> {
-    recorder.stop()
-    console.log(recorder.state)
-    document.getElementById('status').innerHTML = 'recorder stopped'
-    console.log('recorder stopped')
-  }
+    stop.onclick = () => {
+        recorder.stop()
+        console.log(recorder.state)
+        document.getElementById('status').innerHTML = 'recorder stopped'
+        console.log('recorder stopped')
+    }
 
-  video.onloadedmetadata = (e) => {
-    console.log('onloadedmetadata', e)
-  }
+    video.onloadedmetadata = (e) => {
+        console.log('onloadedmetadata', e)
+    }
 
-  recorder.onstop = (e) => {
-    console.log('e', e);
-    console.log('chunks', chunks);
-    const bigVideoBlob = new Blob(chunks, { 'type' : 'video/webm; codecs=webm' })
-    
-    let fd = new FormData();
-    fd.append('fname', 'test.webm');
-    fd.append('data', bigVideoBlob);
+    recorder.onstop = (e) => {
+        console.log('e', e);
+        console.log('chunks', chunks);
+        const bigVideoBlob = new Blob(chunks, { 'type': 'video/webm; codecs=webm' })
 
-    const fetchUrl = '/';
-    const fetchOptions = {
-        method: 'post',
-        body: fd,
-    };
+        let fd = new FormData();
+        fd.append('fname', 'test.webm');
+        fd.append('data', bigVideoBlob);
 
-    fetch(fetchUrl, fetchOptions)
-    .then((response) => {
-        return response.text()
-    })
-    .then((responseText) => {
-        console.log('response text', responseText);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-  }
-}).catch(function(err){
-  console.log('error', err)
+        const fetchUrl = '/';
+        const fetchOptions = {
+            method: 'post',
+            body: fd,
+        };
+
+        fetch(fetchUrl, fetchOptions)
+            .then((response) => {
+                return response.text()
+            })
+            .then((responseText) => {
+                console.log('response text', responseText);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+}).catch(function (err) {
+    console.log('error', err)
 })
